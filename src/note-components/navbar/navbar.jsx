192bff72bf67b1ae9679b9a9c13629110cbd71cb
Jia-A <agrawaljiya2011@gmail.com>
2022-05-12 01:42:06 +0530

    feat : links added and refractor

--- src/note-components/navbar/navbar.jsx
+++ src/note-components/navbar/navbar.jsx
@@ -1,14 +1,27 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../Context/authorization-context";
 import "./navbar.css";
 
 const Navbar = () => {
+const { authDispatch } = useAuth();
+const navigate = useNavigate();
+
+const logoutHandler = () =>{
+navigate("/")
+localStorage.removeItem("token");
+localStorage.removeItem("user");
+authDispatch({ type: "LOGOUT" });
+}
 return (
 
 <nav className="main-nav">
+  <Link to="/" className="link-style link-color-primary">
   <h2 className="brand-name">Dot Notes</h2>
+  </Link>
   <div className="search">
     <input type="text" placeholder="Search notes" className="search-bar" />
   </div>
-  <button className="btn">Filter</button>
+  <button className="btn" onClick={logoutHandler}>Logout</button>
   <div className="avatar-box">
     <img src="./avatar-image.jpg" alt="avatar" className="av-size" />
   </div>
